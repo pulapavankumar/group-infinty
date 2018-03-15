0cafde2bda3183c4448109e27683cb8aea425046
Unknown <manpreeth.bollu@gmail.com>
2018-03-15 21:41:24 +0100

    manpreeth  contribution

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -8,16 +8,8 @@ public class CalculatorMain {
 		return a+b;
 	}
 	public int sub(int a, int b) {
-			return a-b;
-			}
-	public int div(int a, int b) {
-		if(b==0)
-			return b;
-		else
-		return a/b;
-		}
-	public int mul(int a, int b) {
-		return a*b;
+	return a-b;
+
 		}
 		
 	}
