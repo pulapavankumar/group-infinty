bf2be734fafc78e28e3a00932cdc985f8d625814
manpreethb <37306787+manpreethb@users.noreply.github.com>
2018-03-15 13:10:34 +0100

    Update CalculatorMain.java

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,7 +7,17 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
-	public int sub(int a, int b){
-		return a*b;
+	public int sub(int a, int b) {
+			return a-b;
+			}
+		public int mul(int a, int b) {
+			return a*b;
+		}
+		public int div(int a, int b) {
+			if (b==0)
+				return 0;
+			else {}
+				return a/b;
+		
 	}
 }
