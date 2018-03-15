fde08bee80ba898d3e81894985db080ffb6215a5
Unknown <manpreeth.bollu@gmail.com>
2018-03-15 20:41:08 +0100

    my commit

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -10,14 +10,7 @@ public class CalculatorMain {
 	public int sub(int a, int b) {
 			return a-b;
 			}
-		public int mul(int a, int b) {
-			return a*b;
-		}
-		public int div(int a, int b) {
-			if (b==0)
-				return 0;
-			else {}
-				return a/b;
+		
 		
 	}
-}
+
