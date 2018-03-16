b0016bfacca52aa046cffbc29affc47c34bfe6d0
Unknown <pavans1026@gmail.com>
2018-03-16 13:27:58 +0100

    task 1

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -9,6 +9,20 @@ public class CalculatorMain {
 	
 		
 	}
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		if(b==0)
+			return b;
+		else
+			return a/b;
+		
+	}
+	
 }
 	
 	
