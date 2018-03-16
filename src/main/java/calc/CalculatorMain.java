6844374be866a2dcb0437774452f891cb4108b2b
Unknown <pavans1026@gmail.com>
2018-03-16 12:35:53 +0100

    p

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -20,14 +20,9 @@ public class CalculatorMain {
 		else 
 			return a/b;
 	}
-	public class power {
-		public void main() {
-			int x=4,y=3;
-			double result= Math.pow(x,y);
-			System.out.println("x power y is="+result);
-		}
+	
 	}
-}
+
 
 
 	
