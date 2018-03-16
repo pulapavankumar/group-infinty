765428a0a32493d0a086d4b4cf093af9dc349ee7
Unknown <pavans1026@gmail.com>
2018-03-16 14:21:35 +0100

    task 2

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -12,25 +12,18 @@ public class CalculatorMain {
 	public int sub(int a, int b) {
 		return a-b;
 	}
-	public int mul(int a, int b) {
-		return a*b;
-	}
-	public int div(int a, int b) {
-		if(b==0)
-			return b;
-		else
-			return a/b;
-		
-	}
-	public class power {
-		public void main() {
-			int x=4, y=3;
-			double result= Math.pow(x,y);
-			System.out.println(" x power y is="+result);
+		public int (int a, int b) {
+			return ab;
+		}
+		int a , int b {
+			if(b==0)
+				return b
+				else 
+					return a/b
 		}
 	}
 	
-}
+
 	
 	
 
