d5394fb7677ac2653bd47c7b784a344b2f668266
Unknown <pavans1026@gmail.com>
2018-03-16 14:52:48 +0100

    pavan task 2.3

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -17,7 +17,23 @@ public class CalculatorMain {
 	public int mul(int a,int b) {
 		return a*b;
 	}
+	//adding code
+	public int div(int a, int b) {
+		if(b==0)
+			return b;
+		else
+			return a/b;
+		
 	}
+	public class cube{
+		public void main() {
+			int a=3;
+			double result=Math.pow(a,3);
+			System.out.println("cube of the number is="+result);
+		}
+			}
+		}
+	
 	
 
 	
